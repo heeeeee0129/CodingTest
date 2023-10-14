b42ea1b3dac7c3a2d0283ff396a4692e7346cea4
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2023-10-14 12:05:17 +0900

    [level 2] Title: 구명보트, Time: 14.87 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -4,7 +4,12 @@ def solution(people, limit):
     heavy = 0
     light = len(people)-1
     
-    while heavy-light <= 0:
+    # 포인트는 한 구명보트는 한 번에 최대 2명씩밖에 탈 수 없음
+    # -> 두 개의 인덱스를 가리키는 변수 사용
+    # 가장 무거운 애 먼저 태워보고, 가벼운 애 추가 가능하면 걔 넣고 태우기. 안 되면 무거운 애 한 명만
+    
+    
+    while heavy <= light :
         if people[heavy]+people[light] <= limit:
             heavy +=1
             light -=1
