200389cd012885cbdb6794a6e7bccca83d728582
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2023-11-04 22:13:59 +0900

    [level 2] Title: 올바른 괄호, Time: 7.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,13 @@
 def solution(s):
     answer = True
     stack = []
-    for parentheses in s:
-        if parentheses == "(":
+    for i in s:
+        if i == "(":
             stack.append("(")
         else:
             if not stack:
                 return False
             stack.pop()
-    if stack:
-        return False
+   
     
-    return True
+    return not stack
