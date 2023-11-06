13ebebbba6f99a5e6d7ba6ad047f611ecde4f098
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2023-11-06 13:56:54 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,7 @@
 def solution(nums):
-    answer = 0
-    dict = {}
-    
-    for num in nums:
-        dict[num] = 1
-        
-    return min(len(dict), len(nums)//2)
+    # dict = {}
+    # for num in nums:
+    #     dict[num] = 1  
+    # return min(len(dict), len(nums)//2)
+    nums_set = set(nums)
+    return min(len(nums)//2, len(nums_set))
