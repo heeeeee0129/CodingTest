8d25b7f494b9a0b6fe865f8155f30ae99ba8fe95
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2023-11-08 10:20:23 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -3,8 +3,10 @@ import heapq
 def solution(genres, plays):
     answer = []
     dict = {}
-#   딕셔너리 - key: 장르명 , value: heapq에서 배열 [플레이 횟수, i(고유번호)]
+    # dict - key: 장르명 , value: heapq에서 배열 [플레이 횟수, i(고유번호)]
+    
     genre_count = {}
+    # genre_count - 장르별 총 재생횟수 카운트
     
     for i in range(len(genres)):
         if genres[i] in dict:
@@ -16,11 +18,12 @@ def solution(genres, plays):
             genre_count[genres[i]] = plays[i]
     
     genre_count = sorted(genre_count.items(), key=lambda x: x[1], reverse=True)
+    # 많이 재생된 순서대로 장르 정렬
     
     for genre, _ in genre_count:
         top_songs = dict[genre]
         answer.append(heapq.heappop(top_songs)[1])
-        if top_songs:
+        if top_songs: # 여기서 애 먹음. 한 곡만 있는 경우도 고려
             answer.append(heapq.heappop(top_songs)[1])
         
     return answer
