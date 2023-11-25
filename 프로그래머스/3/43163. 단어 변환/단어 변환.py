cc3c35e9bdae4352250f6c94ca7dc85a8d6a81a4
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2023-11-25 14:50:00 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,39 @@
+# 가장 짧은 변환 과정 -> BFS 선택
+# 관건: 한 개의 알파벳만 바꿔 변환 가능한 단어 쌍 판별 
+from collections import deque
+
+def available(w1, w2):
+    count = 0
+    for c1, c2 in zip(w1, w2):
+        if c1 != c2:
+            count+=1
+    return count == 1
+
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    answer = 0
+    
+    # 인접 리스트 만들기. 변환 가능한 단어쌍 저장
+    # 마지막 단어는 begin
+    
+    words.append(begin)
+    graph = [[] for _ in range(len(words)+1)]
+    for i in range(len(words)):
+        for j in range(len(words)):
+            if available(words[i], words[j]):
+                graph[i].append(j)
+                
+    queue = deque([(len(words)-1, 0)]) # begin 단어, 횟수 카운트
+    visited = [False] * len(words)
+    
+    while deque:
+        word, count = queue.popleft()
+        if words[word] == target:
+            return count
+        visited[word] = True
+        for i in graph[word]:
+            queue.append((i, count+1))
+            
+    return 0
