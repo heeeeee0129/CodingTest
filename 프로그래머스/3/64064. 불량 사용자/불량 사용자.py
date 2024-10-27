b0ec771b06b294949e730bc960c9f7eb9e7c78ea
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-10-27 16:27:22 +0900

    [level 3] Title: 불량 사용자, Time: 60.59 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,41 @@
+def dfs(i, visited, n, ban_list, visited_set):
+    global cnt
+    if i >= n:
+        if visited not in visited_set:
+            visited_set.add(visited)
+            cnt = cnt + 1
+        return
+    for b in ban_list[i]:
+        if not (visited >> b) & 1:
+            dfs(i+1, visited | (1 << b), n, ban_list, visited_set)
+
+def solution(user_id, banned_id):
+    global cnt
+    answer = 0
+    n = len(banned_id)
+    ban_list = [[] for _ in range(n)]
+    user_dict = {}
+    visited_set = set()
+    cnt = 0
+    
+    for i, user in enumerate(user_id):
+        if len(user) in user_dict:
+            user_dict[len(user)].append(i)
+        else:
+            user_dict[len(user)] = [i]
+
+    for i, ban in enumerate(banned_id):
+        for target_idx in user_dict[len(ban)]:
+            flag = True
+            for idx, c in enumerate(ban):
+                if c != '*' and c != user_id[target_idx][idx]:
+                    flag = False
+                    break
+            if flag:
+                ban_list[i].append(target_idx)
+                
+    
+    dfs(0, 0, n, ban_list, visited_set)
+            
+    
+    return cnt
