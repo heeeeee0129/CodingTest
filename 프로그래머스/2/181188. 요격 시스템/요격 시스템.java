1410843fd477337f75291ee225a2c08476072d01
heeeeee0129 <86648265+heeeeee0129@users.noreply.github.com>
2024-12-18 16:59:55 +0900

    [level 2] Title: 요격 시스템, Time: 417.83 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,28 +1,25 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
         Arrays.sort(targets, (e1, e2) -> {
-            int ans1 = Integer.compare(e1[1], e2[1]);
+            int ans1 = Integer.compare(e1[0], e2[0]);
             if(ans1 == 0){
-                return Integer.compare(e1[0], e2[0]);
+                return Integer.compare(e1[1], e2[1]);
             }
             return ans1;
         });
         
         int min_end = -1;
         for(int i = 0; i<targets.length; i++){
-            int[] cur = targets[i];
-            
-            if(cur[0] >= min_end){
-                answer++; min_end = cur[1];
+            if(targets[i][0] >= min_end){
+                answer++; min_end = targets[i][1];
+            }else{
+                min_end = Math.min(targets[i][1], min_end);
             }
             
         }
-    
-        
         return answer;
     }
    
